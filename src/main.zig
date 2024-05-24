fd5a87eb55c720f2d343ee47b0ea9ba5a67a24d1
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-05-24 23:31:14 +0700

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,16 @@
 const std = @import("std");
 const net = std.net;
+const Allocator = std.mem.Allocator;
 
 const stdout = std.io.getStdOut().writer();
 
+const SimpleError = "-ERR unknown command\r\n";
+
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const address = try net.Address.resolveIp("127.0.0.1", 6379);
 
     var listener = try address.listen(.{
@@ -16,30 +23,29 @@ pub fn main() !void {
 
         try stdout.print("accepted new connection\n", .{});
 
-        const handler = try std.Thread.spawn(.{}, handleConnection, .{connection});
+        const handler = try std.Thread.spawn(.{}, handleConnection, .{ allocator, connection });
         _ = handler;
     }
 }
 
-pub fn handleConnection(connection: net.Server.Connection) !void {
+pub fn handleConnection(allocator: Allocator, connection: net.Server.Connection) !void {
     defer connection.stream.close();
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
 
     var buffer: [1024]u8 = undefined;
 
     while (true) {
         const msg_size = try connection.stream.read(buffer[0..]);
         const msg = buffer[0..msg_size];
-
-        try stdout.print("received message: {s}\n", .{msg});
-
         const command = try Command.parse(allocator, msg);
 
         switch (command.type) {
             CommandType.PING => try connection.stream.writeAll("+PONG\r\n"),
+            CommandType.ECHO => {
+                const firstArg = getFirstElement(command.args) orelse SimpleError;
+                const response = if (std.mem.eql(u8, firstArg, SimpleError)) firstArg else try toBulkString(firstArg);
+
+                try connection.stream.writeAll(response);
+            },
             else => try connection.stream.writeAll("-ERR unknown command\r\n"),
         }
     }
@@ -47,34 +53,58 @@ pub fn handleConnection(connection: net.Server.Connection) !void {
 
 const CommandType = enum {
     PING,
+    ECHO,
     UNKNOWN,
 };
 
 const Command = struct {
     type: CommandType,
-    args: [][]u8 = undefined,
+    args: std.ArrayList([]const u8),
 
-    pub fn parse(allocator: std.mem.Allocator, msg: []u8) !Command {
-        var split_msg = std.mem.split(u8, msg, "\r\n");
+    pub fn parse(allocator: Allocator, msg: []u8) !Command {
+        var splitMsg = std.mem.split(u8, msg, "\r\n");
+        var args = std.ArrayList([]const u8).init(allocator);
 
-        // Skip array size
-        _ = split_msg.next();
+        const unknownCommand = Command{
+            .type = CommandType.UNKNOWN,
+            .args = args,
+        };
 
-        // Skip string size
-        _ = split_msg.next();
+        // Skip array size and command size
+        _ = splitMsg.next();
+        _ = splitMsg.next();
 
-        const command_type = split_msg.next() orelse return Command{ .type = CommandType.UNKNOWN };
-        var uppercase_command_type = try allocator.alloc(u8, command_type.len);
-        defer allocator.free(uppercase_command_type);
+        const command_type = splitMsg.next() orelse return unknownCommand;
+        var buffer: [1024]u8 = undefined;
+        const uppercaseCommandType = std.ascii.upperString(&buffer, command_type);
 
-        for (command_type, 0..) |c, i| {
-            uppercase_command_type[i] = std.ascii.toUpper(c);
-        }
+        if (std.mem.eql(u8, uppercaseCommandType, "PING")) {
+            return Command{
+                .type = CommandType.PING,
+                .args = args,
+            };
+        } else if (std.mem.eql(u8, uppercaseCommandType, "ECHO")) {
+            while (splitMsg.next()) |arg| {
+                if (std.mem.startsWith(u8, arg, "$")) continue;
+                try args.append(arg);
+            }
 
-        if (std.mem.eql(u8, uppercase_command_type, "PING")) {
-            return Command{ .type = CommandType.PING };
+            return Command{
+                .type = CommandType.ECHO,
+                .args = args,
+            };
         } else {
-            return Command{ .type = CommandType.UNKNOWN };
+            return unknownCommand;
         }
     }
 };
+
+fn toBulkString(string: []const u8) ![]u8 {
+    var buffer: [1024]u8 = undefined;
+    return try std.fmt.bufPrint(&buffer, "${d}\r\n{s}\r\n", .{ string.len, string });
+}
+
+fn getFirstElement(arrayList: std.ArrayList([]const u8)) ?[]const u8 {
+    if (arrayList.items.len == 0) return null;
+    return arrayList.items[0];
+}
